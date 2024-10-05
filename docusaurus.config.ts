b7f4e3e2f406efcfd87a2682c994071a0194b91a
Daniel Huang <daniel.huang@withmantle.com>
2024-10-05 18:50:25 -0400

    initial

---
+++ docusaurus.config.ts
@@ -0,0 +1,58 @@
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
+
+const config: Config = {
+  title: "Daniel Huang",
+  // TODO
+  // favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://danjelhuang.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        blog: false,
+        docs: false,
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "Home",
+    },
+    footer: {
+      style: "dark",
+      copyright: `Built with Docusaurus 🦖`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/index.md
@@ -0,0 +1,33 @@
+---
+title: ""
+---
+
+# Daniel Huang
+
+---
+
+## About Me
+
+🎓 Studying computer engineering at the University of Waterloo
+
+💻 Interested in new grad software engineering roles for 2025
+
+⚽ I like playing and talking about any sport
+
+## Experience
+
+Software engineering internships @
+
+💸 [Mantle](https://withmantle.com/)
+
+🔐 [1Password](https://1password.com/)
+
+🚦 [Miovision](https://miovision.com/)
+
+## Contact
+
+📧 daniel9huang@gmail.com
+
+💡 [LinkedIn](https://www.linkedin.com/in/daniel-huang9/)
+
+🔧 [GitHub](https://github.com/danjelhuang)
